a56ffd036c7343cff7d3e3f30da76e73476d5b4e
CamClrt <camille.clarret@gmail.com>
2022-06-14 21:09:55 +0200

    init template

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Module Custom Template",
+    "depends": [
+        "base",
+        "web",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_users_views.xml",
+        "views/menus.xml",
+    ],
+    "application": True,
+}
---
+++ models/res_users.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+
+    # ---------------------------------------- Private Attributes ---------------------------------
+
+    _inherit = "res.users"
+
+    # --------------------------------------- Fields Declaration ----------------------------------
